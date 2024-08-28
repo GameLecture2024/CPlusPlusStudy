1bd2bd789aa46919f2218dd0a2c0e33a2de772df
GameLecture2024 <dlfjswodlf@naver.com>
2024-08-28 17:47:22 +0900

    (2024.08.28) Player - Monster Battle System

--- RPGBattleSystem/main.cpp
+++ RPGBattleSystem/main.cpp
@@ -20,6 +20,39 @@ int main(void)
 {
 	std::cout << "RPG BATTLE SYSTEM" << std::endl;
 
+	Player player(100, 10);
+	//player.Attack(); 공격 기능 제한
+	player.BeAttacked(10); // 100 - 10 = 90
+	
+	Item sword("소드", 10);
+	Reward slimeReward(50, 1000, sword);
+	Slime slime(50, 5, &slimeReward);
+
+	int userInput = 0;
+	int turn = 1;
+
+	while (true) 
+	{
+		std::cout << "현재 진행 중인 턴 수 : " << turn << std::endl;
+		std::cout << "1번. 공격" << std::endl;
+		std::cout << "2번. ??" << std::endl;
+		std::cin >> userInput;
+		if (userInput == 1)
+		{
+			player.Attack(&slime);
+		}
+
+		std::cout << "슬라임의 Turn" << std::endl;
+		slime.Attack(&player);
+
+		if (player.IsDead() || slime.IsDead())
+		{
+			break;
+		}
+
+		turn++;
+	}
+
 	// 1. 게임 시작 화면
 
 	// 2. 메인 게임 플레이
